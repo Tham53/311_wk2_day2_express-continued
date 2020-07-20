1cb278b90da643bcc0a3b0d97d47694100e38ea3
Tham53 <59705897+Tham53@users.noreply.github.com>
2020-07-19 21:58:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ const contacts = require("./routers/contacts")
 const port = process.env.PORT || 4001
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-//folder contents
+const vehiclesRoutes = require('./routes/vehicles')
+const contactsRoutes = require('./routes/contacts')
+const productsRoutes = require('./routes/products')
+const commentsRoutes = require('./routes/comments')
+
 app.use(products)
 app.use(comments)
 app.use(vehicles)
 app.use(contacts)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.listen(port, () =>
