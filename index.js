e663adb75ee60762c5453674605ff351c354110b
THam53 <texas01988@gmail.com>
2020-07-19 21:32:20 -0500

    new

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
+const express = require('express')
+const app = express()
+const products = require('./routers/products')
+const vehicles = require("./routers/vehicles")
+const comments = require("./routers/comments")
+const contacts = require("./routers/contacts")
+const port = process.env.PORT || 4001
+const bodyParser = require('body-parser')
 
-const port = process.env.PORT || 4001;
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(products)
+app.use(comments)
+app.use(vehicles)
+app.use(contacts)
+app.use(express.static('public'))
 
-app.listen(port, () => {
- console.log(`Web server is listening on port ${port}!`);
-});
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`))
